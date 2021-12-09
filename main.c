90e8b8cacad5997f0a2968163df3c245690a0127
akyito4568 <84411613+akyito4568@users.noreply.github.com>
2021-12-09 17:55:59 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
+    int nbJoueurs,joueurs[nbJoueurs];
+    printf("Combien de joueurs etes vous ? :");
+    do{
+        scanf("%i",&nbJoueurs);
+        if ((nbJoueurs>=1) && (nbJoueurs<=4))
+            break;
+        printf("Entrez un nombre entre 1 et 4 :");
+        }
+    while ((nbJoueurs<1) || (nbJoueurs>4));
+
+
+
 }
