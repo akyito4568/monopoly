4b205a4125d4f0e63d8fcdab1b763c461277d5f4
TitounetteTRue <95620118+TitounetteTRue@users.noreply.github.com>
2021-12-14 17:53:33 +0100

    Create mainjoueurordre.c

---
+++ mainjoueurordre.c
@@ -0,0 +1,9 @@
+#include "mabiblio.h"
+
+int main()
+{
+    int *j1=0,*j2=0,*j3=0,*j4=0;
+    debutejoueur(&j1,&j2,&j3,&j4);
+    printf("j1=%d\t,j2=%d\t,j3=%d\t,j4=%d\t",j1,j2,j3,j4);
+    return 0;
+}
