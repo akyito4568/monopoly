d47a640a388a6813c21517d4a0ba12f13771e2e0
TitounetteTRue <95620118+TitounetteTRue@users.noreply.github.com>
2021-12-14 17:55:30 +0100

    Create mabiblio.h

---
+++ mabiblio.h
@@ -0,0 +1,9 @@
+#ifndef MABIBLIO_H_INCLUDED
+#define MABIBLIO_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void debutejoueur(int*j1, int*j2,int*j3, int*j4);
+
+#endif // MABIBLIO_H_INCLUDED
