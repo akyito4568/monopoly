2a994f90f6acb0f8e7047e5f74b05b0b4916bedc
TitounetteTRue <95620118+TitounetteTRue@users.noreply.github.com>
2021-12-29 12:54:52 +0100

    Create mainmodif.c
    J'ai ajouté avec le code de Robin

---
+++ mainmodif.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+#include <windows.h>
+
+
+
+int main()
+{
+
+    int nbJoueurs;
+    joueur tab[4];
+    t_propriete tab2[32];
+    printf("Combien de joueurs etes vous ? :");
+    do{
+        scanf("%i",&nbJoueurs);
+        if ((nbJoueurs>=2) && (nbJoueurs<=4))
+            break;
+        printf("Entrez un nombre entre 2 et 4 :");
+        }
+    while ((nbJoueurs<2) || (nbJoueurs>4));
+    config_joueur(tab,nbJoueurs);
+    
+    system("cls");
+    remplissagetableau(tab2);
+    InitilisationPlateau(tab2);
+    deplacement_pion(nbJoueurs,tab2,tab);
+    
+    gotoligcol(50,0);
+    ordrejoueurs(nbJoueurs);
+}
