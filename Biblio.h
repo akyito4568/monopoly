e5d97f9e8965369b8911563a42bfd64713d3a882
JulieAllr <95647660+JulieAllr@users.noreply.github.com>
2022-01-04 22:17:07 +0100

    Create Biblio.h

---
+++ Biblio.h
@@ -0,0 +1,77 @@
+#ifndef FONCTIONS_H_INCLUDED
+#define FONCTIONS_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+
+#define MAX_LIMIT 50
+
+typedef struct Propriete{ // définition structure case
+    int place;
+    int groupe;
+    char nomgroupe[MAX_LIMIT];
+    char nom[MAX_LIMIT];
+    int prix;
+    char nomproprietaire[MAX_LIMIT];
+    int loyer;
+    int maison;
+    int hotel;
+    int hypotheque;
+    int x;
+    int y;
+}t_propriete;
+
+typedef struct coord //structure pour coordonnées des joueurs
+{
+    int x,y;
+} coord;
+
+typedef struct joueur //structure des joueurs
+{
+    char nom[MAX_LIMIT];
+    int argent;
+    char signe;
+    int indice;
+    int couleur;
+    int nombre_propri;
+    int position;
+    coord pos_cord;
+} joueur;
+
+//fichier Menu
+void menu(joueur tab[4], t_propriete Plateau[32], int *nbjoueurs, int* choix, joueur tabordre[4]);
+void optionsauvegarde(joueur tab[4], t_propriete Plateau[32], int nbjoueurs, char nomsauvegarde[10]);
+void affichage_regles_jeu();
+
+//fichier Plateau
+t_propriete remplissagetableau();
+void couleurPlateau();
+void colonnecouleur(int couleurtexte, int couleurfond,int positionX,int positionY);
+void bandecouleur(int couleurtexte, int couleurfond,int positionX,int positionY );
+void affichageplateau(t_propriete Plateau[32]);
+void affichagepropriete(t_propriete Plateau[32], joueur* tab);
+
+//fichier Fonctions
+void consoleencouleur(int couleurtexte, int couleurfond);
+void Color(int couleurDuTexte,int couleurDeFond);
+void gotoligcol( int lig, int col);
+void clearScreen();
+
+//fichier Joueurs
+void configuration_joueur(joueur* a);
+void afficher_donnee_joueur_encours(joueur* tab);
+void ordrepassagejoueurs(int nbjoueurs, joueur tab[4], joueur tabapres[4]);
+
+//fichier Dés
+void deplacement_pion(t_propriete tab_p[32],joueur* tab, int i);
+void LanceDe(int * de1,int * de2);
+int doubles(int * a);
+void lancedefinal(joueur* tableau, t_propriete tab[32], int* compteursortie, int* validationprison);
+void lance_de_final(joueur * a, t_propriete tab[32]);
+
+//fichier AchatVente etc
+void achat_location (t_propriete* prop, joueur* a);
+
+
+#endif // FONCTIONS_H_INCLUDED
