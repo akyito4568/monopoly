c8099bf56969a17fe19ce13feacaff090ed547d8
akyito4568 <84411613+akyito4568@users.noreply.github.com>
2022-01-07 10:24:50 +0100

    Add files via upload

---
+++ Fonctions.s
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#include <string.h>
+#include "Biblio.h"
+
+#define MAX_LIMIT 50
+
+void consoleencouleur(int couleurtexte, int couleurfond){
+    int i=0, j=0;
+    for (j=0; j<119; j++){
+        for (i=0; i<60; i++){
+            Color(couleurtexte, couleurfond);
+            gotoligcol(i, j);
+            printf(" ");
+        }
+    }
+}
+
+void Color(int couleurDuTexte,int couleurDeFond) // fonction d'affichage de couleurs
+{
+    HANDLE H=GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(H,couleurDeFond*16+couleurDuTexte);
+}
+void gotoligcol( int lig, int col)
+{
+    COORD mycoord;
+    mycoord.X = col;
+    mycoord.Y = lig;
+    SetConsoleCursorPosition( GetStdHandle( STD_OUTPUT_HANDLE ), mycoord );
+}
+
+void clearScreen(){
+    system("cls");
+    }
--- main.c
+++ main.c
@@ -1,8 +1,137 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#include <string.h>
+#include "Biblio.h"
+
+#define MAX_LIMIT 50
+
+
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int i=0, j=0;
+    int nbjoueurs=0, choix=0, continuer=0, compteur=0, compteursortie=0, validationprison=0,nombre_mort=0;
+    int Case=0;
+    char continuer2 =0;
+    joueur tableau[4]; //ce tableau est rempli dans le menu si on selectionne la nouvelle partie
+    joueur tableauordre[4];
+    t_propriete Plateau[32];
+
+
+    //affichage du menu principal
+    menu(tableau, Plateau, &nbjoueurs, &choix, tableauordre);
+    //si le choix était d'afficher les joueurs ou les regles le menu se réaffiche juste après l'avoir quitté
+    while (choix==4 || choix==5){
+       menu(tableau, Plateau, &nbjoueurs, &choix, tableauordre);
+    }
+
+    ///si le joueur sélectionne une nouvelle partie
+    if (choix==1){
+
+        // remplissage d'un tableau de structure joueur
+        config_joueur(tableau, nbjoueurs);
+
+        //mise en ordre du tableau de structure joueur
+        ordrepassagejoueurs(nbjoueurs, tableau, tableauordre);
+        clearScreen();
+
+        /// remplissage des cases du tableau
+        for (i=0; i<32; i++){
+            remplir_prop(&Plateau[i], i);
+        }
+
+        /// affichage plateau et boucle de parcours pour afficher les pions de chaque joueur
+        affichageplateau(Plateau);
+        for(i=0; i<nbjoueurs; i++){
+            deplacement_pion(Plateau, &tableauordre[i], i);
+        }
+
+        affichagepropriete(Plateau, &tableauordre[compteur]);
+
+        ///démarrage de la partie
+        Color(15,0);
+        gotoligcol(0,85);
+        printf("Appuyer sur entree pour lancer la partie !");
+        gotoligcol(1, 85);
+        printf("On commence avec %s", tableauordre[0].nom);
+        fflush(stdin);
+        scanf("%c", &continuer2);
+
+        do{
+            clearScreen(); //remise à 0 de l'écran à chaque tour
+            gotoligcol(0,0);
+            affichageplateau(Plateau);
+
+            ///lancé des dés, affichage des données du joueur et de la propriété sur laquelle il tombe
+        if ((tableauordre[compteur].position==8) &&(tableauordre[compteur].tour_prison>=0)){
+            prison_final(Plateau,&tableauordre[compteur],tableauordre);
+        }
+
+        if(tableauordre[compteur].tour_prison==-1){
+            lancedefinal(&tableauordre[compteur],Plateau);
+
+        }
+            afficher_donnee_joueur_encours(&tableauordre[compteur]);
+            affichagepropriete(Plateau, &tableauordre[compteur]);
+            //affichage des pions sur le plateau
+            for(i=0; i<nbjoueurs; i++){
+                deplacement_pion(Plateau, &tableauordre[i], i);
+            }
+
+            ///traitement de la case en cours
+            Case = tableauordre[compteur].position;
+            analyse_case_joueur(&Plateau[Case], &tableauordre[compteur], Plateau, tableauordre);
+
+
+            /// réaffichage des données du joueur et de la propriété pour voir les modifications si il y a eu un achat
+            afficher_donnee_joueur_encours(&tableauordre[compteur]);
+            affichagepropriete(Plateau, &tableauordre[compteur]);
+            for(i=0; i<32; i++){
+                for(j=0; j<nbjoueurs; j++){
+                    gotoligcol(0,0);
+                    affichage_bien_joueurs(&Plateau[i], &tableauordre[j]);
+                }
+            }
+
+            Color(15,0);
+            gotoligcol(0,85);
+            printf("Si vous avez termine votre tour selectionner 1 : ");
+            gotoligcol(1, 85);
+            printf("Si vous voulez retourner au menu principal selectionnez 0 : ");
+            fflush(stdin);
+            scanf("%d", &continuer);
+            nombre_mort=0;
+            for (int i=0;i<nbjoueurs;i++){
+                if(tableauordre[i].mort==1){
+                    nombre_mort++;
+                }
+            }
+
+
+            if(continuer==1){
+                do {
+                    compteur=(compteur+1)%nbjoueurs;
+                }
+                while(tableau[compteur].mort==1);
+
+                    }
+            if(continuer==0)
+                break;
+
+        }while(continuer == 1 && nombre_mort!=nbjoueurs-1);
+    }
+        if (nombre_mort==nbjoueurs-1){
+            clearScreen();
+            gotoligcol(0,0);
+            printf("Vous avez gagne");}
+        else {
+        clearScreen();
+        menu(tableau, Plateau, &nbjoueurs, &choix, tableauordre);
+        gotoligcol(50, 0);
+        }
+
+         //affichage du process return en dehors du plateau
 }
+
