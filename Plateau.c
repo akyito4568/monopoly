315762d3cb3a8a9cae0d74618a86aa400b1d8d6e
JulieAllr <95647660+JulieAllr@users.noreply.github.com>
2022-01-08 21:00:40 +0100

    Update Plateau.c

--- Plateau.c
+++ Plateau.c
@@ -33,6 +33,41 @@ void bandecouleur(int couleurtexte, int couleurfond,int positionX,int positionY 
     }
 }
 
+void rectangleCouleur(int couleurtexte, int couleurfond,int positionX,int positionY){
+    int i=0, j=0;
+    for (j=positionX; j<positionX+27; j++){
+        for (i=positionY; i<positionY+4; i++){
+            Color(couleurtexte, couleurfond);
+            gotoligcol(i, j);
+            printf(" ");
+        }
+    }
+}
+
+void colonneCarte(int positionX,int positionY)
+{
+    int i=0, j=0;
+    for (j=positionY; j<positionY+1; j++){
+        for (i=positionX; i<positionX+17; i++){
+            gotoligcol(i, j);
+            printf("%c",0xB3);
+        }
+    }
+
+}
+void colonneCarteProp(int positionX,int positionY)
+{
+    int i=0, j=0;
+    for (j=positionY; j<positionY+1; j++){
+        for (i=positionX; i<positionX+12; i++){
+            gotoligcol(i, j);
+            printf("%c",0xB3);
+        }
+    }
+
+}
+
+
 void affichageplateau(t_propriete Plateau[32])
 {
 
@@ -206,34 +241,141 @@ couleurPlateau();
 void affichagepropriete(t_propriete Plateau[32], joueur* tab) //afficher les données de la case sur laquelle le joueur tombe pendant son tour
 {
     int choixcase=tab->position;
+    int couleur;
     gotoligcol(8, 15);
     Color(15,0);
     //case qui ne sont pas des monuments
-    if(choixcase==0||choixcase==3||choixcase==8||choixcase==11||choixcase==16||choixcase==18||choixcase==24||choixcase==25||choixcase==29)
+    if(choixcase==0||choixcase==3||choixcase==11||choixcase==18||choixcase==25||choixcase==29)
     {
         gotoligcol(9,15);
         printf("%s",Plateau[choixcase].nom);
     }
+    else if(choixcase==8)
+    {
+        Color(15,0);
+        gotoligcol(9,30);
+        printf("%s",Plateau[choixcase].nom);
+        gotoligcol(17,25);
+        printf("____________________________");
+        gotoligcol(18,25);
+        printf("     ||   ||     ||   ||");
+        gotoligcol(19,25);
+        printf("     ||   ||, , ,||   ||");
+        gotoligcol(20,25);
+        printf("     ||  (||/|/(%c||/  ||",0x5C);
+        gotoligcol(21,25);
+        printf("     ||  ||| _'_`|||  ||");
+        gotoligcol(22,25);
+        printf("     ||   || o o ||   ||");
+        gotoligcol(23,25);
+        printf("     ||  (||  - `||)  ||");
+        gotoligcol(24,25);
+        printf("     ||   ||  =  ||   ||");
+        gotoligcol(25,25);
+        printf("     ||   ||%c___/||   ||",0x5C);
+        gotoligcol(26,25);
+        printf("     ||___||) , (||___||");
+        gotoligcol(27,25);
+        printf("    /||---||-%c_/-||---||%c ",0x5C,0x5C);
+        gotoligcol(28,25);
+        printf("   / ||--_||_____||_--|| %c ",0x5C);
+        gotoligcol(29,25);
+        printf("  (_(||)-| S123-45 |-(||)_)");
+    }
+    else if(choixcase==24)
+    {
+        Color(15,0);
+        gotoligcol(9,30);
+        printf("%s",Plateau[choixcase].nom);
+        gotoligcol(19,28);
+         printf("     ,   /%c   ,",0x5C);
+         gotoligcol(20,28);
+         printf("    / '-'  '-' %c ",0x5C);
+         gotoligcol(21,28);
+         printf("   |   POLICE   |");
+         gotoligcol(22,28);
+         printf("   %c    .--.    /",0x5C);
+         gotoligcol(23,28);
+         printf("    |  ( 19 )  |");
+         gotoligcol(24,28);
+         printf("    %c   '--'   /",0x5C);
+         gotoligcol(25,28);
+         printf("     '--.  .--'");
+         gotoligcol(26,28);
+         printf("         %c/",0x5C);
+
+    }
+    else if(choixcase==16)
+    {
+        Color(15,0);
+        gotoligcol(9,30);
+        printf("%s",Plateau[choixcase].nom);
+        gotoligcol(19,25);
+        printf("        __-------__");
+        gotoligcol(20,25);
+        printf("      / _---------_ %c ",0x5C);
+        gotoligcol(21,25);
+        printf("     / /           %c %c ",0x5C,0x5C);
+        gotoligcol(22,25);
+        printf("     | |           | |");
+        gotoligcol(23,25);
+        printf("     |_|___________|_|");
+        gotoligcol(24,25);
+        printf(" /-%c|                 |/-%c ",0x5C,0x5C);
+        gotoligcol(25,25);
+        printf("| _ |%c       0       /| _ |",0x5C);
+        gotoligcol(26,25);
+        printf("|(_)| %c      !      / |(_)|",0x5C);
+        gotoligcol(27,25);
+        printf("|___|__%c_____!_____/__|___|",0x5C);
+        gotoligcol(28,25);
+        printf("[_________|ECE22|_________]");
+        gotoligcol(29,25);
+        printf(" ||||    ~~~~~~~~     ||||");
+        gotoligcol(30,25);
+        printf(" `--'                 `--'");
+
+    }
     else //case monuments
     {
-        gotoligcol(10,15);
-        printf("Pays: %s", Plateau[choixcase].nomgroupe);
-        gotoligcol(11,15);
-        printf("Monument: %s", Plateau[choixcase].nom);
-        gotoligcol(12,15);
-        printf("Prix d achat: %d euros", Plateau[choixcase].prix);
-        gotoligcol(13,15);
+        gotoligcol(9,26);
+        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",0xDA,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xBF);
+        gotoligcol(22,26);
+        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",0xC0,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xD9);
+
+        colonneCarteProp(10,26);
+        colonneCarteProp(10,54);
+        couleur=Plateau[choixcase].groupe;
+        rectangleCouleur(0,couleur,27,10);
+
+        gotoligcol(10,32);
+        printf("%s", Plateau[choixcase].nomgroupe);
+        gotoligcol(12,30);
+        printf("%s", Plateau[choixcase].nom);
+
+        Color(15,0);
+
+        gotoligcol(14,28);
+        printf("Prix d achat:   %d euros", Plateau[choixcase].prix);
+        gotoligcol(15,28);
         printf("Proprietaire: %s", Plateau[choixcase].nomproprietaire);
-        gotoligcol(14,15);
-        printf("Prix visite: %d euros", Plateau[choixcase].loyer);
-        gotoligcol(15,15);
-        printf("Maison: %d", Plateau[choixcase].maison);
-        gotoligcol(16,15);
-        printf("Hotel: %d", Plateau[choixcase].hotel);
-        gotoligcol(17,15);
-        printf("Hypotheque: %d euros", Plateau[choixcase].hypotheque);
+        gotoligcol(16,28);
+        printf("Prix visite:     %d euros", Plateau[choixcase].loyer);
+        gotoligcol(17,28);
+        printf("Maison:                %d", Plateau[choixcase].maison);
+        gotoligcol(18,28);
+        printf("Hotel:                 %d", Plateau[choixcase].hotel);
+        gotoligcol(19,30);
+        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4,0xC4);
+        gotoligcol(20,31);
+        printf("Hypotheque :");
+        gotoligcol(21,33);
+        printf("%d euros", Plateau[choixcase].hypotheque);
+
+
     }
 }
+
 
 
 
@@ -252,11 +394,13 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=0;
         pro->x=8;
         pro->y=5;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
 
     case 1:
         pro->place = 2;
-        pro->groupe = 1;
+        pro->groupe = 13;
         strcpy(pro->nomgroupe, "Russie");
         strcpy(pro->nom, "Place Rouge");
         pro->prix=60;
@@ -267,10 +411,12 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=16;
         pro->y=5;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 2:
         pro->place = 3;
-        pro->groupe = 1;
+        pro->groupe = 13;
         strcpy(pro->nomgroupe, "Russie");
         strcpy(pro->nom, "Bolchoi");
         pro->prix=60;
@@ -281,6 +427,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=24;
         pro->y=5;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 3:
         pro->place = 4;
@@ -295,6 +443,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=0;
         pro->x=32;
         pro->y=5;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 4:
         pro->place = 5;
@@ -309,6 +459,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=0;
         pro->x=40;
         pro->y=5;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 5:
         pro->place = 6;
@@ -323,6 +475,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=48;
         pro->y=5;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 6:
         pro->place = 7;
@@ -337,6 +491,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=56;
         pro->y=5;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 7:
         pro->place = 8;
@@ -351,6 +507,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=64;
         pro->y=5;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 8:
         pro->place = 9;
@@ -365,10 +523,12 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=0;
         pro->x=72;
         pro->y=5;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 9:
         pro->place = 10;
-        pro->groupe = 3;
+        pro->groupe = 14;
         strcpy(pro->nomgroupe, "Bresil");
         strcpy(pro->nom, "Christ Redempteur");
         pro->prix=140;
@@ -379,10 +539,12 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=72;
         pro->y=10;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 10:
         pro->place = 11;
-        pro->groupe = 3;
+        pro->groupe = 14;
         strcpy(pro->nomgroupe, "Bresil");
         strcpy(pro->nom, "Cathedrale Metropolitana");
         pro->prix=140;
@@ -393,6 +555,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=72;
         pro->y=15;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 11:
         pro->place = 12;
@@ -407,6 +571,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=0;
         pro->x=72;
         pro->y=20;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 12:
         pro->place = 13;
@@ -421,10 +587,12 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=0;
         pro->x=72;
         pro->y=25;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 13:
         pro->place = 14;
-        pro->groupe = 3;
+        pro->groupe = 14;
         strcpy(pro->nomgroupe, "Bresil");
         strcpy(pro->nom,"Theatre Amazonas");
         pro->prix=160;
@@ -435,10 +603,12 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=72;
         pro->y=30;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 14:
         pro->place = 15;
-        pro->groupe = 5;
+        pro->groupe = 8;
         strcpy(pro->nomgroupe, "Etats-Unis");
         strcpy(pro->nom, "Statue de la Liberte");
         pro->prix=180;
@@ -449,10 +619,12 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=72;
         pro->y=35;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 15:
         pro->place = 16;
-        pro->groupe = 5;
+        pro->groupe = 8;
         strcpy(pro->nomgroupe, "Etats-Unis");
         strcpy(pro->nom, "Mont Rushmore");
         pro->prix=180;
@@ -463,6 +635,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=72;
         pro->y=40;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 16:
         pro->place = 17;
@@ -477,10 +651,12 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=0;
         pro->x=72;
         pro->y=45;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 17:
         pro->place = 18;
-        pro->groupe = 5;
+        pro->groupe = 8;
         strcpy(pro->nomgroupe, "Etats-Unis");
         strcpy(pro->nom, "Grand Canyon");
         pro->prix=200;
@@ -491,6 +667,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=64  ;
         pro->y=45;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 18:
         pro->place = 19;
@@ -505,10 +683,12 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=0;
         pro->x=56;
         pro->y=45;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 19:
         pro->place = 20;
-        pro->groupe = 6;
+        pro->groupe = 5;
         strcpy(pro->nomgroupe, "Italie");
         strcpy(pro->nom, "Colisee");
         pro->prix=220;
@@ -519,6 +699,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=48;
         pro->y=45;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 20:
         pro->place = 21;
@@ -533,10 +715,12 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=0;
         pro->x=40;
         pro->y=45;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 21:
         pro->place = 22;
-        pro->groupe = 6;
+        pro->groupe = 5;
         strcpy(pro->nomgroupe, "Italie");
         strcpy(pro->nom, "Tour de Pise");
         pro->prix=220;
@@ -547,10 +731,12 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=32;
         pro->y=45;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 22:
         pro->place = 23;
-        pro->groupe = 6;
+        pro->groupe = 5;
         strcpy(pro->nomgroupe, "Italie");
         strcpy(pro->nom, "Cathedrale de Milan");
         pro->prix=240;
@@ -561,10 +747,12 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=24;
         pro->y=45;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 23:
         pro->place = 24;
-        pro->groupe = 7;
+        pro->groupe = 6;
         strcpy(pro->nomgroupe, "Japon");
         strcpy(pro->nom, "Skytree");
         pro->prix=260;
@@ -575,6 +763,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=16;
         pro->y=45;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 24:
         pro->place = 25;
@@ -589,6 +779,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=0;
         pro->x=8;
         pro->y=45;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 25:
         pro->place = 26;
@@ -603,10 +795,12 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=8;
         pro->y=40;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 26:
         pro->place = 27;
-        pro->groupe = 7;
+        pro->groupe = 6;
         strcpy(pro->nomgroupe, "Japon");
         strcpy(pro->nom, "Plais Imperial de Kyoto");
         pro->prix=260;
@@ -617,10 +811,12 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=8;
         pro->y=35;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 27:
         pro->place = 28;
-        pro->groupe = 7;
+        pro->groupe = 6;
         strcpy(pro->nomgroupe, "Japon");
         strcpy(pro->nom, "Palais Imperial de Tokyo");
         pro->prix=280;
@@ -631,6 +827,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=8;
         pro->y=30;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 28:
         pro->place = 29;
@@ -645,6 +843,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=0;
         pro->x=8;
         pro->y=25;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 29:
         pro->place = 30;
@@ -659,10 +859,12 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=0;
         pro->x=8;
         pro->y=20;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 30:
         pro->place = 31;
-        pro->groupe = 8;
+        pro->groupe = 1;
         strcpy(pro->nomgroupe, "France");
         strcpy(pro->nom, "Arc de Triomphe");
         pro->prix=300;
@@ -673,10 +875,12 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=8;
         pro->y=15;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
     case 31:
         pro->place = 32;
-        pro->groupe = 8;
+        pro->groupe = 1;
         strcpy(pro->nomgroupe, "France");
         strcpy(pro->nom, "Tour Eiffel");
         pro->prix=320;
@@ -687,6 +891,8 @@ void remplir_prop(t_propriete* pro, int i){
         pro->hypotheque=30;
         pro->x=8;
         pro->y=10;
+        pro->proprietaire=4;
+        pro->prix_actuel=0;
         break;
 
     }
@@ -695,17 +901,38 @@ void remplir_prop(t_propriete* pro, int i){
 
 void couleurPlateau()
 {
-    //Communauté
+    //Debut
+    bandecouleur(0,15,2,5);
+    //prison
+    bandecouleur(0,15,2, 69);
+    // Parking
+    bandecouleur(0,15,42,69);
+    //Police
+    bandecouleur(0,15,42,5);
 
-    bandecouleur(0,9,17,69);
-    colonnecouleur(0,9,18,69);
-    colonnecouleur(0,9,18,74);
-    bandecouleur(0,9,20,69);
+    //Aeroport
 
-    bandecouleur(0,9,40,5);
-    bandecouleur(0,9,37,5);
-    colonnecouleur(0,9,38,5);
-    colonnecouleur(0,9,38,10);
+    bandecouleur(0,9,2,37);
+    bandecouleur(0,9,5,37);
+    colonnecouleur(0,9,3,37);
+    colonnecouleur(0,9,3,42);
+
+    bandecouleur(0,9,22,69);
+    colonnecouleur(0,9,23,69);
+    colonnecouleur(0,9,23,74);
+    bandecouleur(0,9,25,69);
+
+    bandecouleur(0,9,42,37);
+    bandecouleur(0,9,45,37);
+    colonnecouleur(0,9,43,37);
+    colonnecouleur(0,9,43,42);
+
+    bandecouleur(0,9,22,5);
+    bandecouleur(0,9,25,5);
+    colonnecouleur(0,9,23,5);
+    colonnecouleur(0,9,23,10);
+
+
 
     //Russie
 
@@ -743,6 +970,5 @@ void couleurPlateau()
     bandecouleur(0,1,12,5);
     bandecouleur(0,1,7,5);
 
-    bandecouleur(0,15,4,5);
-}
 
+}
